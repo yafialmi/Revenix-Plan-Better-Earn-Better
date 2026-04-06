c2287079391ca9f4e72ab83b1b0dfe3b201b6537
yafialmi <yafialmi@gmail.com>
2026-04-06 16:55:21 +0700

    [6][Added Gitignore and Added Front End Projects]

---
+++ FE/revenix/src/App.jsx
@@ -0,0 +1,49 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+
+function App() {
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#333",
+            color: "#fff",
+          },
+          success: {
+            style: {
+              background: "green",
+            },
+          },
+          error: {
+            style: {
+              background: "red",
+            },
+          },
+        }}
+      />
+      
+      <Routes>
+        {/* 
+      Routes berfungsi untuk inisiasi link navigasi yang ada pada web.
+      Contoh: www.website.com/(path)
+      Element berfungsi untuk memberi tahu React, isi konten halaman yang kita navigasikan itu yang mana sebagai contoh element Login akan ngeload function Login.
+      */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+}
+
+{
+  /* 
+  Selalu definisikan export pada js, agar fungsinya dapat dikenali, export terbagi dua yaitu default (nama fungsi) atau {(nama fungsi), (nama fungsi)}, array digunakan apabila kita ingin menggunakan lebih dari satu function pada page tersebut. */
+}
+
+export default App;
---
+++ FE/revenix/src/services/auth/authService.js
@@ -0,0 +1,16 @@
+import { axiosInstanceDefault } from "../../api/axiosInstance";
+
+export const authUser = async (email, password) => {
+  {
+    /* Berfungsi untuk logic login ke website kita, membutuhkan dua parameter berupa email dan password user, fungsi tersebut akan mengembalikan respon dari backend dalam bentuk JSON */
+  }
+  try {
+    const response = await axiosInstanceDefault.post("/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
